637b242b935a6d8afa436d06ba6d63adaa92f2f3
nitinsingh <nitinsingh@localhost>
2023-12-21 17:56:07 +0530

    added

--- firstWebApp/src/main/java/com/ntsGroup/springboot/firstWebApp/login/LoginController.java
+++ firstWebApp/src/main/java/com/ntsGroup/springboot/firstWebApp/login/LoginController.java
@@ -1,6 +1,5 @@
 package com.ntsGroup.springboot.firstWebApp.login;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,27 +13,9 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @SessionAttributes("name") // this will made available in all models
 public class LoginController {
 
-	@Autowired
-	private AuthenticationService authenticationService;
-
 	@RequestMapping(value="login", method = RequestMethod.GET)
-	public String gotoLoginPage() {
-		return "login";
-	}
-
-	@PostMapping("login")
-	public String gotoWelcomePage(@RequestParam String name , @RequestParam String password, ModelMap model) { //password . name must be same as the name declared in jsp
-
-		boolean validUser = authenticationService.authenticate(name, password);
-
-		if (validUser) {
-
-			model.put("name", name);
-			model.put("password", password);
-			return "welcome";
-			
-		} else {
-			model.put("errorMessage", "Invalid User!!!");
-			return "login";}
+	public String gotoLoginPage(ModelMap model) {
+		model.put("name", "in28Minutes");
+		return "welcome";
 	}
 }
